92f58e6f8658145c095f2eb3278bea35bfba71f9
gregoryhooks <gregoryhooksjr@gmail.com>
2017-02-22 07:44:48 -0600

    Changed content type

--- chameleon/chameleonObjectStorage.py
+++ chameleon/chameleonObjectStorage.py
@@ -6,7 +6,7 @@ import json
 def putObject(token_id, fileName, objectData):
     url = globalVars.chameleonObjectStorageURL + "/" + globalVars.chameleonContainerName + "/" + fileName
 
-    my_headers = {"Content-Type": 'text/html', "X-Auth-Token": token_id}
+    my_headers = {"Content-Type": 'binary/octet-stream', "X-Auth-Token": token_id}
 
     r = requests.put(url, data=objectData, headers=my_headers)
     return r
